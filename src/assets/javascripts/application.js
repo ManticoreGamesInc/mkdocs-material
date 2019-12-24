a982cabac2cf0e62a154d408b6bfbe8c8e592406
Benjamin Staneck <bstaneck@manticoregames.com>
2019-12-24 18:29:54 +0100

    New take on the nav hiding feature

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -284,6 +284,7 @@ function initialize(config) { // eslint-disable-line func-style
     new Material.Event.Listener("[data-md-component=query]", [
       "focus", "keyup", "change"
     ], new Material.Search.Result("[data-md-component=result]", () => {
+      config.url.base = config.url.base.replace(/\/$/, "")
       return fetch(`${config.url.base}/search/search_index.json`, {
         credentials: "same-origin"
       }).then(response => response.json())
