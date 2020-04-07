2b5fc5b14d842eada88f4dba0cf6edf3fdf2b369
Benjamin Staneck <bstaneck@manticoregames.com>
2020-04-07 20:33:40 +0200

    Generate

--- material/precache-manifest.d717f88f7fa366eaa1c8d120f2b5ac26.js
+++ material/precache-manifest.d717f88f7fa366eaa1c8d120f2b5ac26.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69a744ffd6e64353b046bac82bfa58bd",
+    "revision": "1ed49eba81fead59513f2fb7fa365634",
     "url": "404.html"
   },
   {
@@ -52,179 +52,179 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/fonts/specimen/MaterialIcons-Regular.woff2"
   },
   {
-    "revision": "fe145990006e4e0ebbcc10317db155ef",
+    "revision": "b7dc087e414e7a2e487f18836cd4f7f9",
     "url": "assets/icons/apple-splash-1125-2436.png"
   },
   {
-    "revision": "c7fd04b73a62e89e7a6d49f18f220a1a",
+    "revision": "0ca06f8e90f99a23f16536fd2449a636",
     "url": "assets/icons/apple-splash-1136-640.png"
   },
   {
-    "revision": "6a6473fc75aa2de4a4414fe258646e4f",
+    "revision": "48ec461564e2f56ffb191bff447f91db",
     "url": "assets/icons/apple-splash-1242-2208.png"
   },
   {
-    "revision": "ca105f650666f1b2490cec376de2bc10",
+    "revision": "3add0dabfa7d7b5df2855abca81ca731",
     "url": "assets/icons/apple-splash-1242-2688.png"
   },
   {
-    "revision": "42b89582626b32e4fdb21520dee230ef",
+    "revision": "b87422c8041cf3f0538eaa7e026877c9",
     "url": "assets/icons/apple-splash-1334-750.png"
   },
   {
-    "revision": "23569bf62b64ba19c072ed4b48ca7e22",
+    "revision": "24c889b05be6c9e6771977ab1959a192",
     "url": "assets/icons/apple-splash-1536-2048.png"
   },
   {
-    "revision": "f3c8b9aa7497b24b35be00526f3d56fb",
+    "revision": "9cd10030fec24c2b73520b4afd65c81c",
     "url": "assets/icons/apple-splash-1668-2224.png"
   },
   {
-    "revision": "ab854524d66d41aad5ef500222001c2f",
+    "revision": "50377c886163949febd038d7df1696c6",
     "url": "assets/icons/apple-splash-1668-2388.png"
   },
   {
-    "revision": "703a8bd42f24a21386bc2f4d82d2f04f",
+    "revision": "0c65a72a77e8b778721c67204dcb3511",
     "url": "assets/icons/apple-splash-1792-828.png"
   },
   {
-    "revision": "a91ca5c1cb8970cfe6699b3bd8cf9e59",
+    "revision": "d03c56c174023ba1e321afe86ad6fed2",
     "url": "assets/icons/apple-splash-2048-1536.png"
   },
   {
-    "revision": "8cc80aa60aceceae3423e83dbf287370",
+    "revision": "aea1d9d19eaed580d8817c1d88e16653",
     "url": "assets/icons/apple-splash-2048-2732.png"
   },
   {
-    "revision": "dfe5712e71e9522e4e2a40a6de23e99d",
+    "revision": "2827b0f7e0cb5d8510706fc3c2d1d357",
     "url": "assets/icons/apple-splash-2208-1242.png"
   },
   {
-    "revision": "d0658f5e7b16d0f618042b2fd2ece35b",
+    "revision": "b059438b2f5b609d2dc2cf7beca7e235",
     "url": "assets/icons/apple-splash-2224-1668.png"
   },
   {
-    "revision": "5053bb1d9220d0324e5218be85aee829",
+    "revision": "0c0fdf539a0df191f7f7e0c3068ce78b",
     "url": "assets/icons/apple-splash-2388-1668.png"
   },
   {
-    "revision": "5a7e70b739af9c0352f75e67afebb335",
+    "revision": "63d77dbb740554b9bbd765e02a13f799",
     "url": "assets/icons/apple-splash-2436-1125.png"
   },
   {
-    "revision": "fdfc41bdcf24016c2b4e43c0865e9c90",
+    "revision": "c323dee3a7ac371a33f03a3200567c91",
     "url": "assets/icons/apple-splash-2688-1242.png"
   },
   {
-    "revision": "6c9e219bce1256b3fd240bda5372dd16",
+    "revision": "803495641abe6a8c3b4d7dca2a88acba",
     "url": "assets/icons/apple-splash-2732-2048.png"
   },
   {
-    "revision": "1d9e320a8c89188d356c932a47de47f3",
+    "revision": "747b8d551c1434f672e2cfead4ad0cbc",
     "url": "assets/icons/apple-splash-640-1136.png"
   },
   {
-    "revision": "c48fab1ad99e97c4147e9d28d01a5c19",
+    "revision": "5335f11c58a459c9dc20fa86d79a9f37",
     "url": "assets/icons/apple-splash-750-1334.png"
   },
   {
-    "revision": "97abe8ba1819de9845a20dcc939463b7",
+    "revision": "5e0008a5d0be79aa8b821b91bf56ee9f",
     "url": "assets/icons/apple-splash-828-1792.png"
   },
   {
-    "revision": "6263c9309edf15c6387da1d2f81f5c26",
+    "revision": "e8042c3c8c6b214a23a52f0faf36bae6",
     "url": "assets/icons/apple-splash-dark-1125-2436.png"
   },
   {
-    "revision": "f289843ff6f9bfb6e54d0025cfd4fcb1",
+    "revision": "8428c47986be734769734aa9c54aebf3",
     "url": "assets/icons/apple-splash-dark-1136-640.png"
   },
   {
-    "revision": "f119f7f71f8a286d88083b4d4859c829",
+    "revision": "7460e47895e55dd0b89fa66ac63222e1",
     "url": "assets/icons/apple-splash-dark-1242-2208.png"
   },
   {
-    "revision": "0a667cbe8424f1aeaa15d61627093183",
+    "revision": "6f50f3a693dcca1e5a8cdd98eacbdf12",
     "url": "assets/icons/apple-splash-dark-1242-2688.png"
   },
   {
-    "revision": "23316d5184f9811d541ceb0597ffb960",
+    "revision": "cf317467093f7bca9952b6b432053529",
     "url": "assets/icons/apple-splash-dark-1334-750.png"
   },
   {
-    "revision": "5736d9b0afa21ac09e8925263670eadc",
+    "revision": "53508e8b0a70249448daac56330e3e64",
     "url": "assets/icons/apple-splash-dark-1536-2048.png"
   },
   {
-    "revision": "233e64cf0a859ec8b36b9d6de12f11ef",
+    "revision": "d5f137182bce81f0b6475b50ffc358b8",
     "url": "assets/icons/apple-splash-dark-1668-2224.png"
   },
   {
-    "revision": "75f47af288e442bb7203ce194081c897",
+    "revision": "fc773287f794070897143f327d655f58",
     "url": "assets/icons/apple-splash-dark-1668-2388.png"
   },
   {
-    "revision": "3554d968e52f1d272264dbecba63176b",
+    "revision": "c2d86ec1399982fa29201dc53596eef1",
     "url": "assets/icons/apple-splash-dark-1792-828.png"
   },
   {
-    "revision": "dd42c36e59e99dcf77f97b86c164bada",
+    "revision": "41357fbd3b6094861193d112af62f0b6",
     "url": "assets/icons/apple-splash-dark-2048-1536.png"
   },
   {
-    "revision": "de5476b8a9bf94f9ce3aa309c81d033e",
+    "revision": "9d1a8474e5b4de9f031513b966553a1e",
     "url": "assets/icons/apple-splash-dark-2048-2732.png"
   },
   {
-    "revision": "b6fc0defa259d961ea0913f5664d7c6f",
+    "revision": "214246f8c1f6d06fd61a6a91d9b30015",
     "url": "assets/icons/apple-splash-dark-2208-1242.png"
   },
   {
-    "revision": "1099d2fa60be15294c9f40db5097e30b",
+    "revision": "f54c960d7cf01cddd7bdfb4593c8cb47",
     "url": "assets/icons/apple-splash-dark-2224-1668.png"
   },
   {
-    "revision": "94d8cfdb95f9f95d32f6efef11f5b6ba",
+    "revision": "e595e63e4306d87c3244899a15d46c40",
     "url": "assets/icons/apple-splash-dark-2388-1668.png"
   },
   {
-    "revision": "df418e5d2bd45e6ecbad5d9140cad9b9",
+    "revision": "7b1f12a86457bc4dbfb1288057a6c9af",
     "url": "assets/icons/apple-splash-dark-2436-1125.png"
   },
   {
-    "revision": "ed8e5312f7e57c47de5aeeb1bb267303",
+    "revision": "5d68bd45e8535fd49632cbd3ab213a94",
     "url": "assets/icons/apple-splash-dark-2688-1242.png"
   },
   {
-    "revision": "66e66d2f1e517dfa5ed3726332724056",
+    "revision": "460856c099d7d8783867c2d593377db4",
     "url": "assets/icons/apple-splash-dark-2732-2048.png"
   },
   {
-    "revision": "90837b19f706ea67f507f67595b7a157",
+    "revision": "0dd7f70defd7e39c32c99266e8df372e",
     "url": "assets/icons/apple-splash-dark-640-1136.png"
   },
   {
-    "revision": "77ef925ae474d29afacce3cce78a3303",
+    "revision": "fb84a071c7afc2e91e59977e508766ef",
     "url": "assets/icons/apple-splash-dark-750-1334.png"
   },
   {
-    "revision": "3a7a4ee44790d0078c88b48956f2a511",
+    "revision": "f7c287375f41393f27ac8b821306d0a3",
     "url": "assets/icons/apple-splash-dark-828-1792.png"
   },
   {
-    "revision": "007f7c5de40230a27d5c301a371491c9",
+    "revision": "b41712314e4c9f0d129039a676c24d9f",
     "url": "assets/icons/apple-touch-icon-120.png"
   },
   {
-    "revision": "a3688a7c6364ce86633da935fbce5ea9",
+    "revision": "f96bf89eed61ad847b64bc2e99149770",
     "url": "assets/icons/apple-touch-icon-152.png"
   },
   {
-    "revision": "a94683f3db64e47deb71faccdc2c0810",
+    "revision": "5aa1b8ea8abb072deae86d347bbffb6e",
     "url": "assets/icons/apple-touch-icon-167.png"
   },
   {
-    "revision": "17cffe43381bbdee10377725ebedbc66",
+    "revision": "acdbcbc7e2103a7f1e34752eb342a82e",
     "url": "assets/icons/apple-touch-icon.png"
   },
   {
@@ -232,31 +232,31 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/icons/browserconfig.xml"
   },
   {
-    "revision": "ff8ea5a95b3e9f2981d5a666165308e8",
+    "revision": "05886a3f651698fc46afcba09bddd19f",
     "url": "assets/icons/favicon-16x16.png"
   },
   {
-    "revision": "7ed15d240ac5cde4de98a0b517394382",
+    "revision": "b19d75483bd9a71cc65fb5fe67685065",
     "url": "assets/icons/favicon-192x192.png"
   },
   {
-    "revision": "07b92066d74195aaf3cf204c2c26b8c0",
+    "revision": "1665f80bc6ea1507e0ca2a9aca932179",
     "url": "assets/icons/favicon-196x196.png"
   },
   {
-    "revision": "12a3660c52bf5ed5e853b00ffa654b3b",
+    "revision": "a8d7c93aa9f202d90ef73f7e0ffba6db",
     "url": "assets/icons/favicon-32x32.png"
   },
   {
-    "revision": "795a08d49ad692f15fd060d0297fb3b8",
+    "revision": "57c8aaf4db17b2393048ebecaa6eb13f",
     "url": "assets/icons/favicon-512x512.png"
   },
   {
-    "revision": "e7cf734d844b5507e9b22631de090600",
+    "revision": "388b14ea1556004dc74f15d5c64fe110",
     "url": "assets/icons/mstile-150x150.png"
   },
   {
-    "revision": "25fb265921c978af730908c339625e38",
+    "revision": "f738c4576f27896989060b18058f70d9",
     "url": "assets/icons/safari-pinned-tab.svg"
   },
   {
@@ -337,7 +337,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "assets/webfonts/fa-solid-900.woff2"
   },
   {
-    "revision": "aae2cff9512997b237f70b3e31006bcd",
+    "revision": "df5761406f7336a6d82ce79c3ffe65e5",
     "url": "offline.html"
   }
 ]);
--- material/sw.js
+++ material/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.73745a26ed946a4e6e98621a331e6cf7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.d717f88f7fa366eaa1c8d120f2b5ac26.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
